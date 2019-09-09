d028dc536c3b9ca2e744a2a2fca100ef414998d9
13mehul <54508171+13mehul@users.noreply.github.com>
2019-09-09 12:37:40 +0530

    Add files via upload

---
+++ DSASET2/PQS2Q2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  float x;
+  cin>>x;
+  cout<<sin(x);
+}
---
+++ DSASET2/PQS2Q3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi 3.142857
+
+int main()
+{
+  float r;
+  cin>>r;
+  cout<<"Circumference = "<<(2*pi*r)<<setprecision(4)<<"\n";
+  cout<<"Area = "<<(pi*r*r)<<setprecision(4)<<"\n";
+}
