1e5b627fe9cc20e14a6005fe5622e3ae25fa6fc1
biprajit <biprajit121@gmail.com>
2019-10-25 23:28:45 +0530

    Proper Indentation is Given Wherever Necessary

--- DSASET3/PQS3Q12.cpp
+++ DSASET3/PQS3Q12.cpp
@@ -12,7 +12,7 @@ class Detail
 class Birthday
 {
   public :
-  int date,month,year;
+  int date, month, year;
 };
 
 class Age : public Birthday
@@ -24,7 +24,7 @@ class Age : public Birthday
     age = 2019 - year;
     if(month > 9)
     age++;
-    cout<<"Age is = "<<age<<"\n";
+    cout << "Age is = "<<age<<"\n";
   }
 };
 
@@ -33,19 +33,19 @@ class Student : public Detail , public Birthday
   public :
   void Read()
   {
-    cout<<"Enter the name \n";
-    cin>>name;
-    cout<<"Enter roll no. \n";
-    cin>>roll_no;
-    cout<<"Enter dob\n";
-    cin>>date>>month>>year;
+    cout << "Enter the name \n";
+    cin >> name;
+    cout << "Enter roll no. \n";
+    cin >> roll_no;
+    cout << "Enter dob\n";
+    cin >> date >> month >> year;
   }
 
   void Print()
   {
-    cout<<"Name : "<<name<<"\n";
-    cout<<"rollno. :"<<roll_no<<"\n";
-    cout<<"DOB : "<<date<<"-"<<month<<"-"<<year<<"\n";
+    cout << "Name : " << name << "\n";
+    cout << "rollno. :" << roll_no << "\n";
+    cout << "DOB : "<< date <<"-" << month << "-" << year <<"\n";
   }
 };
 bool Compare(Student const &ptr1 , Student const &ptr2)
@@ -56,32 +56,32 @@ bool Compare(Student const &ptr1 , Student const &ptr2)
 int main()
 {
   Student obj1[5];
-  for(int i = 0;i<5;i++)
+  for(int i = 0; i < 5; i++)
   obj1[i].Read();
 
-  cout<<"Enter case\n";
+  cout << "Enter case\n";
   int case1;
-  cin>>case1;
+  cin >> case1;
 
   switch(case1)
   {
-    case 1 : cout<<"Sorting \n";
-              sort(obj1,obj1+5,Compare);
+    case 1 : cout << "Sorting \n";
+              sort( obj1, obj1 + 5, Compare);
 
-              for(int i = 0;i<5;i++)
+              for(int i = 0; i < 5; i++)
               obj1[i].Print();
               break;
 
-    case 2 :   cout<<"FInd roll no :\n";
+    case 2 :   cout << "FInd roll no :\n";
                 int x;
-                cin>>x;
-                for(int i = 0;i<5;i++)
+                cin >> x;
+                for(int i = 0; i < 5; i++)
                 {
                   if(obj1[i].roll_no == x)
                   {
-                    cout<<"roll no = "<<obj1[i].roll_no<<"\n";
-                    cout<<"name = "<<obj1[i].name<<"\n";
-                    cout<<"DOB = "<<obj1[i].date<<"-"<<obj1[i].month<<"-"<<obj1[i].year<<"\n";
+                    cout << "roll no = " << obj1[i].roll_no << "\n";
+                    cout << "name = " << obj1[i].name << "\n";
+                    cout << "DOB = " << obj1[i].date << "-" << obj1[i].month << "-" << obj1[i].year << "\n";
                   }
                 }
                 break;
